9a2eb474aaf9784d0a73d8b5c6c274d9208fedfb
root <root@Rodrick.localdomain>
2020-05-17 18:04:38 -0400

    AGBPrint working!

--- One/source/Main.cpp
+++ One/source/Main.cpp
@@ -1,7 +1,10 @@
 #include <gba.h>
 #include <stdio.h>
 #include <type_traits>
+
+extern "C" {
 #include "AGBPrint.h"
+}
 
 static int DEFUALT_MODE_CONSTANT = MODE_3 | BG2_ON;
 
@@ -15,14 +18,15 @@ int main( void )
   SetMode( currentMode );
   const auto MAX_COLOR_CONSTANT = 0xFF00;
   typename std::remove_const< decltype( MAX_COLOR_CONSTANT ) >::type currentColor = 0x0000;
-  u16 i = 23;
-  AGBPrintf( "Hello gameboy %d!", i );
+  for( size_t i = 0; i < ( SCREEN_WIDTH * SCREEN_HEIGHT ); ( ( u16* ) VRAM )[ i++ ] = 0xFF00 );
+  //u16 k = 23;
+  AGBPrintf( "Hello gameboy!" );//, k );
   while( true )
-  {
-//    for( size_t i = 0; i < ( SCREEN_WIDTH * SCREEN_HEIGHT ); ( ( u16* ) VRAM )[ i++ ] = 
+//  {
+  //  for( size_t i = 0; i < ( SCREEN_WIDTH * SCREEN_HEIGHT ); ( ( u16* ) VRAM )[ i++ ] = Test() );
 //        ( ( currentColor > MAX_COLOR_CONSTANT ) ? ( currentColor = 0x0000 ) : currentColor++ ) );
     SystemCall( 5 );
-  }
+//  }
   return 0;
 }
 
