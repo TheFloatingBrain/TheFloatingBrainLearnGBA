956c0b2784a430d46e5330bbc974b4f640b03fea
TheFloatingBrain <TheFloatingBrain@gmail.com>
2020-06-07 03:40:42 -0400

    herp

---
+++ Three/assets/Cross.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(Cross)
+
+//======================================================================
+//
+//	Cross, 16x16@2, 
+//	+ palette 256 entries, not compressed
+//	+ 4 tiles not compressed
+//	Total size: 512 + 64 = 576
+//
+//	Time-stamp: 2020-06-07, 03:39:44
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.16
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_CROSS_H
+#define GRIT_CROSS_H
+
+#define CrossTilesLen 64
+extern const unsigned char CrossTiles[64];
+
+#define CrossPalLen 512
+extern const unsigned short CrossPal[256];
+
+#endif // GRIT_CROSS_H
+
+//}}BLOCK(Cross)
